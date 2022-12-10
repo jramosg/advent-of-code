c2f451afcc9610fa7945b8030df0016494b0d6c4
j-ramos <jonurnieta@gmail.com>
2022-12-10 16:25:49 +0100

    Day 09

--- src/advent_of_code/year_2022/day_09/main.clj
+++ src/advent_of_code/year_2022/day_09/main.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]
             [clojure.set :as set]))
 
-(def ^:const ^:private moves
+(def ^:const ^:private deltas
   {"R" [1 0]
    "L" [-1 0]
    "U" [0 1]
@@ -35,85 +35,62 @@
 (defn move [prev-pos f steps]
   (+ prev-pos (* f steps)))
 
-(defn- passed-from [dir steps {[hx hy] :h :as current-position}]
-  (let [[x-move y-move] (get moves dir)]
-    (loop [steps (range 1 (inc (util/str->int steps)))
-           position current-position
-           tail-seen #{}]
-      (if-let [step (first steps)]
-        (let [head-pos [(move hx x-move step)
-                        (move hy y-move step)]
-              tail-pos (chase-head head-pos (:t position))]
-          (recur
-            (rest steps)
-            {:h head-pos :t tail-pos}
-            (conj tail-seen tail-pos)))
-        {:position position
-         :tail-seen tail-seen}))))
-
-(defn part-1 [& [data]]
-  (loop [moves (or data (util/slurp+split-line "09"))
-         current-pos {:h [0 0] :t [0 0]}
-         tail-visits #{[0 0]}]
-     (if-let [[dir steps] (some-> moves first (str/split #" "))]
-       (let [{:keys [position tail-seen]}
-             (passed-from dir steps current-pos)]
-         (recur
-           (rest moves)
-           position
-           (set/union tail-visits tail-seen)))
-       (count tail-visits))))
-
-;;;;;;;;;;;;;;;;
-;;;; PART 2 ;;;;
-;;;;;;;;;;;;;;;;
-
-(defn- passed-from2 [dir steps current-position]
-  (let [[x-move y-move] (get moves dir)]
-    (loop [steps (range 1 (inc (util/str->int steps)))
-           {:keys [current-position tail-seen]}
-           {:current-position current-position
-            :tail-seen #{}}]
-      (if (seq steps)
-        (recur (rest steps)
-               (loop [i 0
-                      current-position current-position
-                      tail-positions tail-seen]
-                 (cond
-                   (not (get current-position i))
-                   {:current-position current-position
-                    :tail-seen tail-positions}
-                   (not (get current-position (dec i)))
-                   (recur (inc i)
-                          (update current-position i
-                                  (fn [[hx hy]]
-                                    [(move hx x-move 1)
-                                     (move hy y-move 1)]))
-                          tail-positions)
-                   :else
-                   (let [head-pos (get current-position (dec i))
-                         tail-pos (chase-head head-pos (get current-position i))]
-                     (recur (inc i)
-                            (assoc current-position
-                              i tail-pos)
-                            (if (= i 9)
-                              (conj tail-positions tail-pos)
-                              tail-positions))))))
+(defn- loop-tails [{:keys [current-position tail-seen]} dir knots]
+  (let [[x-move y-move] (get deltas dir)]
+    (loop [i 0
+           current-position current-position
+           tail-positions tail-seen]
+      (cond
+        (not (get current-position i))
         {:current-position current-position
-         :tail-seen tail-seen}))))
+         :tail-seen tail-positions}
+        (not (get current-position (dec i)))
+        (recur (inc i)
+               (update current-position i
+                       (fn [[hx hy]]
+                         [(move hx x-move 1)
+                          (move hy y-move 1)]))
+               tail-positions)
+        :else
+        (let [head-pos (get current-position (dec i))
+              tail-pos (chase-head head-pos (get current-position i))]
+          (recur (inc i)
+                 (assoc current-position
+                   i tail-pos)
+                 (if (= i (dec knots))
+                   (conj tail-positions tail-pos)
+                   tail-positions)))))))
 
-(defn part-2 [& [data]]
-  (loop [moves (or data (util/slurp+split-line "09"))
+(defn- passed-from2 [dir steps current-position knots]
+  (loop [steps (range 1 (inc (util/str->int steps)))
+         {:keys [current-position tail-seen] :as status}
+         {:current-position current-position
+          :tail-seen #{}}]
+    (if (seq steps)
+      (recur
+        (rest steps)
+        (loop-tails status dir knots))
+      {:current-position current-position
+       :tail-seen tail-seen})))
+
+(defn part-* [knots data]
+  (loop [moves data
          current-pos (reduce
                        (fn [m i]
                          (assoc m i [0 0]))
-                      {} (range 10))
+                      {} (range knots))
          tail-visits #{[0 0]}]
     (if-let [[dir steps] (some-> moves first (str/split #" "))]
       (let [{:keys [current-position tail-seen]}
-            (passed-from2 dir steps current-pos)]
+            (passed-from2 dir steps current-pos knots)]
         (recur
           (rest moves)
           current-position
           (set/union tail-visits tail-seen)))
       (count tail-visits))))
+
+(defn part-1 [& [data]]
+  (part-* 2 (or data (util/slurp+split-line "09"))))
+
+(defn part-2 [& [data]]
+  (part-* 10 (or data (util/slurp+split-line "09"))))
